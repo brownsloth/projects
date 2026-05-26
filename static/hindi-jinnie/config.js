79f3ad1b490104216718d37ba9a33ad2f69882c4
brownsloth <brownsloth18@gmail.com>
2026-05-27 00:51:19 +0530

    Adding support for the trained translation model project.

---
+++ static/hindi-jinnie/config.js
@@ -0,0 +1,3 @@
+// Local dev default. On Netlify, set this in your Gatsby build or override in config.prod.js
+window.HINDI_JINNIE_API =
+  "https://transformerbasedenglishtohindi-production.up.railway.app";
